9685788fe3b6e28bceecb3dff4059d88612c7add
Енотайзер <amisitysu14@gmail.com>
2024-07-01 18:56:48 +0300

    first commit

---
+++ src/com/amisitysu/gmail/Person.java
@@ -0,0 +1,52 @@
+package com.amisitysu.gmail;
+
+
+class Person {
+    private String name;
+    private int age;
+    private String profession;
+
+    public Person(String name, int age, String profession) {
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String newProfession) {
+        this.profession = newProfession;
+    }
+
+    public void displayInfo() {
+        System.out.println("Ім'я: " + name + ", Вік: " + age + ", Професія: " + profession);
+    }
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person("John", 30, "Інженер");
+        Person person2 = new Person("Mary", 25, "Вчитель");
+        Person person3 = new Person("Bob", 35, "Лікар");
+
+        person1.displayInfo();
+        person2.displayInfo();
+        person3.displayInfo();
+
+        Person person4 = new Person("Alice", 28, "Архітектор");
+        person4.displayInfo();
+
+        person4.setProfession("Дизайнер");
+
+        person4.displayInfo();
+    }
+}
